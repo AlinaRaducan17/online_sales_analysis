3a593afce052ad1596ca28f5b5a757adca5974bb
Alina Serban <alina.decembrie@gmail.com>
2025-02-21 13:44:41 +0200

    Stergere linii cod si modificare cantitati

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
         Product("Carte electronica", 44.45, 10) 
     ]
 
-for product in products:
-        print(product.display_info())
+# for product in products:
+#         print(product.display_info())
 
 print(products[0].update_quantity(5))
 print(products[1].update_quantity(2))
@@ -21,11 +21,9 @@ for product in products:
     product_manager.add_product(product)
     
 product_manager.add_product(Product("Caiet", 10.5, 2))
-product_manager.display_products()
-
-print(f"Valoarea totală a inventarului: {product_manager.total_spent()} RON")
+# product_manager.display_products()
 
 product_manager.remove_product("Stilou")
-product_manager.display_products()
+# product_manager.display_products()
 
  
